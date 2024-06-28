0979065634dce0090ba6a7046678c7d012d6dcfd
Elzar <elzarjoldoshev92@gmail.com>
2024-06-28 10:13:54 +0600

    elzar18

--- women/models.py
+++ women/models.py
@@ -31,7 +31,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_slug': self.slug})
+        return reverse('category', kwargs={'cat_id': self.pk})
 
     class Meta:
         verbose_name = 'Категория'
